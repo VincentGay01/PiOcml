3a2abc5b1a1ac314215bb9e6d2524973c8c48018
VincentGay01 <101732915+VincentGay01@users.noreply.github.com>
2023-04-18 13:40:08 +0200

    Add files via upload

--- BBP.ml
+++ BBP.ml
@@ -1,17 +1,17 @@
-let rec BBP (nombre:float) (it:float) (stop:float) =
-if it =0. then BBP (((1. /. (16. ** 0. )  ) *. (( 4. /. ((8. *. 0. ) +. 1.) ) -. (2. /. ((8. *. 0. ) +. 4.))-. (1. /. ((8. *. 0. ) +. 5.))-. (1. /. ((8. *. 0. ) +. 6.)))  ) ) (1.) stop 
+let rec bbp (nombre:float) (it:float) (stop:float) =
+if it =0. then bbp (((1. /. (16. ** 0. )  ) *. (( 4. /. ((8. *. 0. ) +. 1.) ) -. (2. /. ((8. *. 0. ) +. 4.))-. (1. /. ((8. *. 0. ) +. 5.))-. (1. /. ((8. *. 0. ) +. 6.)))  ) ) (1.) stop 
 else if it=stop then nombre
   else 
-    BBP (nombre +. ((1. /. (16. ** it ) ) *. (( 4. /. ((8. *. (it ) ) +. 1. ) ) -. (2. /. ((8. *. (it ) ) +. 4.))-. (1. /. ((8. *. (it ) ) +. 5.))-. (1. /. ((8. *. (it ) ) +. 6.)))  ) ) (it +. 1.) stop ;;
+    bbp (nombre +. ((1. /. (16. ** it ) ) *. (( 4. /. ((8. *. (it ) ) +. 1. ) ) -. (2. /. ((8. *. (it ) ) +. 4.))-. (1. /. ((8. *. (it ) ) +. 5.))-. (1. /. ((8. *. (it ) ) +. 6.)))  ) ) (it +. 1.) stop ;;
 
 
-let auxBBP stop =
-  BBP 0. 0. stop ;;
+let auxbbp stop =
+  bbp 0. 0. stop ;;
 
   
-let plop = auxBBP 1. ;;
-let plop = auxBBP 2. ;;
-let plop = auxBBP 3. ;;
-let plop = auxBBP 4. ;;
-let plop = auxBBP 5. ;;
-let plop = auxBBP 1000000. ;;
+let plop = auxbbp 1. ;;
+let plop = auxbbp 2. ;;
+let plop = auxbbp 3. ;;
+let plop = auxbbp 4. ;;
+let plop = auxbbp 5. ;;
+let plop = auxbbp 1000000. ;;
--- gouteagoute.ml
+++ gouteagoute.ml
@@ -1,8 +1,4 @@
- (*let rec  gouteagoute (nombre:float) (it:float) (stop:float) =
- if it=0.  then  gouteagoute (2. +. (1. /. 3. )) 1. stop 
- else if it=stop then  nombre
- else
-  gouteagoute ( 2. +. ((it +. 1. ) /. (2. *. (it +. 1. ) +. 1. )) *. nombre)  (it +. 1. ) stop  ;;*)
+
 
   let rec gouteagoutev2 (nombre:float) (it:float) (stop:float) =
     if it=stop then gouteagoutev2 (2. +. ((it +. 1.) /. (2. *. (it +. 1.) +. 1. ) ) ) (stop -. 1.) stop 
@@ -11,12 +7,12 @@
         gouteagoutev2 ( 2. +. (((it -. 1. ) /. (2. *. (it -. 1. ) +. 1. )) *. nombre))  (it -. 1. ) stop  ;;
 
 
-let AlgoGoute stop =
+let algoGoute stop =
   gouteagoutev2 (0.) (stop) stop ;;
 
+let pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679 ;;
 
-
-let x= AlgoGoute 1000. ;;
-let x= AlgoGoute 100. ;;
-let x= AlgoGoute 50. ;;
-let x= AlgoGoute 25. ;;
+let x= algoGoute 1000. ;;
+let x= algoGoute 100. ;;
+let x= algoGoute 50. ;;
+let x= algoGoute 25. ;;
